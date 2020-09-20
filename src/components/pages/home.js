f16f728d590c1349bc8e9880c36475e240338a13
Stevemmmmm <estebangarcia2121@gmail.com>
2020-09-20 16:24:06 -0700

    Basic type in animation and fade in for subtitle

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -4,21 +4,25 @@ import Card from "./../objects/Card";
 
 class Homepage extends React.Component {
     render() {
-        const cardOneText = "Sample text in here!!!!!!!!!!!";
+        const artistsDescription = "Join Game Development Club to get to work with other artists or something";
+        const musiciansDescription = "";
+        const programmersDescription = "Sample text in here!!!!!!!!!!!";
+        const animatorsDescription = "";
+        const writersDescription = "";
 
         return (
             <div>
                 <div className="section section-one">
-                    <div className="title-text">Game Development Club</div>
-                    <div className="subtitle-text">Making games at Franklin High School since 2020</div>
+                    <div className="typing-effect title-text">Game Development Club</div>
+                    <div className="subtitle-text fade-in">Making games at Franklin High School since 2020</div>
                 </div>
 
                 <div className="section">
-                    <Card title="Artists" x="171px" y="150px" text={cardOneText}/>
-                    <Card title="Musicians" x="471px" y="350px" text={cardOneText} />
-                    <Card title="Programmers" x="771px" y="150px" text={cardOneText} />
-                    <Card title="Animators" x="1071px" y="350px" text={cardOneText} />
-                    <Card title="Writers" x="1371px" y="150px" text={cardOneText} />
+                    <Card title="Artists" x="171px" y="150px" text={artistsDescription}/>
+                    <Card title="Musicians" x="471px" y="350px" text={musiciansDescription} />
+                    <Card title="Programmers" x="771px" y="150px" text={programmersDescription} />
+                    <Card title="Animators" x="1071px" y="350px" text={animatorsDescription} />
+                    <Card title="Writers" x="1371px" y="150px" text={writersDescription} />
                 </div>
             </div>
         );
