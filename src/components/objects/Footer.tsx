b231d9daadf724452a2bed1c647f2223370b7194
Stevemmmmm <estebangarcia2121@gmail.com>
2020-09-21 17:39:44 -0700

    Migrated to Typescript

---
+++ src/components/objects/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+    return (
+        <div>
+            <footer className="footer text">
+                <p>Some contents</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
