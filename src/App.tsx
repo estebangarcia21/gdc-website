75396459f10f2f34c18813fac340e46929775ec2
Stevemmmmm <estebangarcia2121@gmail.com>
2020-09-26 19:48:35 -0700

    Created routing for the join gdc page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Homepage from './assets/components/pages/home';
 import Todopage from './assets/components/pages/to-do';
 import Ourgamespage from './assets/components/pages/our-games';
+import Joingdcpage from './assets/components/pages/join-gdc';
 
 import './assets/scss/App.css';
 
@@ -14,6 +15,7 @@ const App: React.FC = () => {
                 <Route exact path='/' component={Homepage} />
                 <Route exact path='/to-do' component={Todopage} />
                 <Route exact path='/our-games' component={Ourgamespage} />
+                <Route exact path='/join-gdc' component={Joingdcpage} />
             </Router>
         </div>
     );
---
+++ src/assets/components/pages/join-gdc.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Joingdcpage: React.FC = () => {
+    return (
+        <div>
+            <div>EEE</div>
+        </div>
+    );
+};
+
+export default Joingdcpage;
