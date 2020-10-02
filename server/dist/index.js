a18ece138fb15cd22cd1a7f154f3a32afe2a2362
Stevemmmmm <estebangarcia2121@gmail.com>
2020-10-02 08:21:57 -0700

    Refreshed dist file for back end and remove unecassary context class

--- server/dist/index.js
+++ server/dist/index.js
@@ -12,13 +12,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
+const express_1 = __importDefault(require("express"));
+require("reflect-metadata");
 const type_graphql_1 = require("type-graphql");
-const GameResolver_1 = require("./resolvers/GameResolver");
 const typeorm_1 = require("typeorm");
 const Game_1 = require("./entities/Game");
+const GameResolver_1 = require("./resolvers/GameResolver");
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     yield typeorm_1.createConnection({
         type: 'postgres',
--- server/src/Context.ts
+++
@@ -1,5 +0,0 @@
-import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core';
-
-export type Context = {
-    em: EntityManager<IDatabaseDriver<Connection>>;
-};
