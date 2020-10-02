20d10ad38e76d6f0429e73e62795ae042af7e996
Stevemmmmm <estebangarcia2121@gmail.com>
2020-10-02 11:17:57 -0700

    Removed all body text in the homepage

--- website/src/pages/home.tsx
+++ website/src/pages/home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import concept_art from '../assets/images/concept_art.png';
 
 const Homepage: React.FC = () => {
     return (
@@ -13,63 +12,21 @@ const Homepage: React.FC = () => {
                     <div className='subtitle-text fade-in white'>
                         Making Games at Franklin High School Since 2020
                     </div>
-                    <img
-                        src={concept_art}
-                        width='40%'
-                        height='40%'
-                        className='horizontal-center gdc-artist-img box-shadow gradient-outline fade-in'
-                        alt='Art Sample'
-                    />
                 </div>
             </div>
 
             <div className='page-content'>
                 <div className='information-section-header'>
-                    Collaborate. Create. Learn.
+                    Everything you need to know in one place.
                 </div>
 
-                <div className='text'>
-                    Be part of a growing team of developers, artists, musicians,
-                    writers, and animators. Learn and collaborate with members
-                    to create a quality game.{' '}
-                    <span className='pop-text'>No previous</span>
-                    <br />
-                    <span className='pop-text'>experience needed.</span>
-                </div>
-
-                <div className='text'>
-                    Learn all the skills you need to become a successful
-                    contributor to our projects at Game Development Club.{' '}
-                    <span className='pop-text'>
-                        No paid applications or licenses
-                    </span>{' '}
-                    are needed to be a successful person in the game development
-                    industry.
-                </div>
-
-                <div className='text'>
-                    Express your creative ideas on the drawing board. If you
-                    have an idea for a game, let it out at Game Development
-                    Club's meetings! <span className='pop-text'>All</span>
-                    <br />
-                    <span className='pop-text'>ideas are welcome</span> as long
-                    as they are school appropriate.
-                </div>
+                <Link
+                    to='join-gdc'
+                    className='header-button-join-now--large horizontal-center'
+                >
+                    Join GDC
+                </Link>
             </div>
-
-            <div className='subtitle-text--black'>
-                Join Game Development Club
-            </div>
-            <div className='text horizontal-text-center'>
-                Join today and become part of a team that you can enjoy.
-            </div>
-
-            <Link
-                to='join-gdc'
-                className='header-button-join-now--large horizontal-center'
-            >
-                Join GDC
-            </Link>
         </div>
     );
 };
