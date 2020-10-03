889dc19842a91857001c20d6d7ed4b9140723dfc
Stevemmmmm <estebangarcia2121@gmail.com>
2020-10-02 19:10:40 -0700

    Layed out basic theme for webpage (4th and final iteration)

--- website/src/pages/home.tsx
+++ website/src/pages/home.tsx
@@ -9,24 +9,36 @@ const Homepage: React.FC = () => {
                     <div className='title-text fade-in'>
                         Game Development Club
                     </div>
-                    <div className='subtitle-text fade-in white'>
-                        Making Games at Franklin High School Since 2020
+                    <div className='subtitle-text--centered fade-in white'>
+                        The Community of Communities at Franklin High School
                     </div>
                 </div>
             </div>
 
             <div className='page-content'>
                 <div className='information-section-header'>
-                    Everything you need to know in one place.
+                    Building our Community
                 </div>
 
-                <Link
-                    to='join-gdc'
-                    className='header-button-join-now--large horizontal-center'
-                >
-                    Join GDC
-                </Link>
+                <div className='horizontal-bar' />
+
+                <p className='text'>
+                    Game Development Club is always looking for artists,
+                    programmers, writers, musicians, and animators to make up
+                    our community and team at Game Development Club.
+                </p>
             </div>
+
+            <p className='subtitle-text--black subtitle-text--centered'>
+                Join Game Development Club
+            </p>
+
+            <Link
+                to='join-gdc'
+                className='header-button-join-now--large horizontal-center'
+            >
+                Join GDC
+            </Link>
         </div>
     );
 };
