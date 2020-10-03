c0347796597ad8d53c77403ac455c853d8ce22b5
Stevemmmmm <estebangarcia2121@gmail.com>
2020-10-02 20:13:49 -0700

    Completed basic homepage template and added section components

--- website/src/pages/home.tsx
+++ website/src/pages/home.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+interface InfoSectionProps {
+    title: string;
+}
+
+const InfoSection: React.FC<InfoSectionProps> = (props) => {
+    return (
+        <div>
+            <div className='page-content'>
+                <div className='information-section-header'>{props.title}</div>
+
+                <div className='horizontal-bar' />
+
+                <p className='text'>{props.children}</p>
+            </div>
+        </div>
+    );
+};
+
 const Homepage: React.FC = () => {
     return (
         <div>
@@ -15,30 +33,36 @@ const Homepage: React.FC = () => {
                 </div>
             </div>
 
-            <div className='page-content'>
-                <div className='information-section-header'>
-                    Building our Community
-                </div>
+            <InfoSection title='BUILDING OUR COMMUNITY'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. A ad
+                consequatur porro ipsam. Vitae provident dolore iste, corrupti
+                magnam possimus id voluptas quisquam odio distinctio odit
+                accusantium exercitationem ipsam accusamus?
+            </InfoSection>
+
+            <InfoSection title='SAMPLE TEXT'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+                quam officia, illum iure dolorum explicabo doloremque provident
+                ipsam earum voluptas dignissimos at perferendis ipsa sapiente
+                magnam, sit necessitatibus eligendi delectus.
+            </InfoSection>
+
+            <div id='pre-footer'>
+                <p className='subtitle-text horizontal-text-center'>
+                    JOIN GAME DEVELOPMENT CLUB
+                </p>
 
                 <div className='horizontal-bar' />
 
-                <p className='text'>
-                    Game Development Club is always looking for artists,
-                    programmers, writers, musicians, and animators to make up
-                    our community and team at Game Development Club.
-                </p>
+                <p className='text'>Join Game Development Club today!</p>
+
+                <Link
+                    to='join-gdc'
+                    className='header-button-join-now horizontal-center'
+                >
+                    Join GDC
+                </Link>
             </div>
-
-            <p className='subtitle-text--black subtitle-text--centered'>
-                Join Game Development Club
-            </p>
-
-            <Link
-                to='join-gdc'
-                className='header-button-join-now--large horizontal-center'
-            >
-                Join GDC
-            </Link>
         </div>
     );
 };
