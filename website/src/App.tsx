60c0b17fa66854492007299a9da377933d29a333
Stevemmmmm <estebangarcia2121@gmail.com>
2020-10-04 12:42:05 -0700

    Created basic website layout in home.tsx

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './assets/scss/App.css';
 import Homepage from './pages/home';
-import Ourgamespage from './pages/our-games';
 
 const App: React.FC = () => {
     return (
         <div>
             <Router>
                 <Route exact path='/' component={Homepage} />
-                <Route exact path='/our-games' component={Ourgamespage} />
             </Router>
         </div>
     );
--- website/src/pages/home.tsx
+++ website/src/pages/home.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import concept_art from '../assets/images/concept_art.png';
 
 const Homepage: React.FC = () => {
-    return <div></div>;
+    return (
+        <div>
+            <div id='home-banner'>
+                <div className='vertical-center'>
+                    <div id='home-banner-left-panel'>
+                        <p className='title'>Game Development Club</p>
+                        <p className='subtitle'>
+                            The Community of Communities at Franklin
+                        </p>
+                    </div>
+                </div>
+
+                <div id='home-banner-right-panel'>
+                    {/* Fade swap images */}
+                    <img
+                        className='box-shadow'
+                        src={concept_art}
+                        width='700px'
+                        height='auto'
+                        alt='Images from our games'
+                    />
+                </div>
+            </div>
+
+            <p className='title center-text'>What is Game Development Club?</p>
+        </div>
+    );
 };
 
 export default Homepage;
