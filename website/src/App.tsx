e66b4c22733a5301a89e87aeb3a8ae22299031d0
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-01 19:46:05 -0800

    Added redux store

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,26 +1,33 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { createStore } from '@reduxjs/toolkit';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './assets/scss/App.css';
 import Home from './pages/Home';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import OurGames from './pages/OurGames';
 import Resources from './pages/Resources';
+import { directoryReducer } from './stores/directory/reducers';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
 });
 
+const store = createStore(directoryReducer);
+
 const App: React.FC = () => {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/our-games' component={OurGames} />
-          <Route path='/to-do' component={Resources} />
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/our-games' component={OurGames} />
+            <Route path='/resources' component={Resources} />
+          </Switch>
+        </Router>
+      </Provider>
     </ApolloProvider>
   );
 };
