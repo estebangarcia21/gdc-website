ea00e214921fd6847cbf8c5e0f314af56055c14f
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-01 22:26:51 -0800

    Added resource page header

---
+++ website/src/components/ResourcePageHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface ResourcePageHeaderProps {
+  title: string;
+}
+
+const ResourcePageHeader: React.FC<ResourcePageHeaderProps> = ({ title }) => {
+  return <div>{title}</div>;
+};
+
+export default ResourcePageHeader;
---
+++ website/src/pages/resource-pages/programmers/GettingstartedProgrammers.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ResourcePageHeader from '../../../components/ResourcePageHeader';
+
+interface Props {}
+
+const GettingstartedProgrammers: React.FC<Props> = () => {
+  return (
+    <div>
+      <ResourcePageHeader title='Getting Started | Programmers' />
+    </div>
+  );
+};
+
+export default GettingstartedProgrammers;
