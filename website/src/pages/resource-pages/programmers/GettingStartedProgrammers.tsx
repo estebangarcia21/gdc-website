d3533cba619e40f55e4b090fff7c0bc3d67fe202
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-03 14:15:43 -0800

    Remade bugged file

---
+++ website/src/pages/resource-pages/programmers/GettingStartedProgrammers.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResourcePageHeader from '../../../components/ResourcePageHeader';
+
+const GettingStartedProgrammers: React.FC = () => {
+  return (
+    <div>
+      <ResourcePageHeader title='Getting Started | Programmers' />
+    </div>
+  );
+};
+
+export default GettingStartedProgrammers;
