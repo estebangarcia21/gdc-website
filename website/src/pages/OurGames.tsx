50a348d3526ab0b28ee3a7e9166628bd6ce7b49e
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-04 13:23:03 -0800

    Added query interfaces

--- website/src/pages/OurGames.tsx
+++ website/src/pages/OurGames.tsx
@@ -12,33 +12,40 @@ const GET_GAMES = gql`
   }
 `;
 
+interface Game {
+  id: number;
+  name: string;
+  description: string;
+  year: string;
+}
+
+interface GameData {
+  games: Game[];
+}
+
+interface Todo {
+  id: number;
+  group: string;
+  name: string;
+  completed: boolean;
+}
+
 const OurGames: React.FC = () => {
-  const { data, loading, error } = useQuery<{
-    games: {
-      id: number;
-      name: string;
-      description: string;
-      year: string;
-    }[];
-  }>(GET_GAMES);
+  const { data, loading } = useQuery<GameData>(GET_GAMES);
 
   if (loading) return <div>Loading...</div>;
 
-  if (error) {
-    console.log(error.message);
-    return <div>Query request error!</div>;
-  }
-
   return (
-    <div>
-      {data?.games.map(game => (
-        <div key={game.id}>
-          <p>{game.name}</p>
-          <p>{game.year}</p>
-          <p>{game.description}</p>
-          <br />
-        </div>
-      ))}
+    <div style={{ backgroundColor: 'black' }}>
+      {data &&
+        data.games.map(game => (
+          <div key={game.id}>
+            <p>{game.name}</p>
+            <p>{game.year}</p>
+            <p>{game.description}</p>
+            <br />
+          </div>
+        ))}
     </div>
   );
 };
