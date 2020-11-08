6e5bc8e9371c294744e5dd775cacb5b20684c585
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-08 14:06:50 -0800

    Added toggleCompletion mutation

--- server/src/resolvers/TodoResolver.ts
+++ server/src/resolvers/TodoResolver.ts
@@ -1,23 +1,34 @@
-import { Arg, Query, Resolver } from 'type-graphql';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 import Todo from '../entities/Todo';
 
 @Resolver()
 export default class TodoResolver {
   @Query(() => [Todo])
   async todos(): Promise<Todo[]> {
-    return Todo.find();
+    return await Todo.find();
   }
 
   @Query(() => [Todo])
   async getTodosByTeam(@Arg('team') team: string): Promise<Todo[]> {
-    return Todo.find({
+    return await Todo.find({
       where: {
-        team: team,
+        team,
       },
       order: {
         completed: 'ASC',
         title: 'ASC',
       },
     });
+  }
+
+  @Mutation(() => Todo)
+  async toggleCompletion(@Arg('id') id: number): Promise<Todo | undefined> {
+    const todo = await Todo.findOne({ where: { id } });
+
+    if (!todo) throw new Error('Todo does not exist.');
+
+    Object.assign(todo, { completed: !todo.completed });
+
+    return await todo?.save();
   }
 }
