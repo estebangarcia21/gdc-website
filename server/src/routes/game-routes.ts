b62766b960bd54e7bdc8555d6e004153522d2618
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-10 01:16:29 -0800

    Implemented game fetching by title and id along with its tags

--- server/src/routes/game-routes.ts
+++ server/src/routes/game-routes.ts
@@ -1,9 +1,45 @@
 import { Router } from "express";
+import { client } from "../pg-client";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("EMZICALE");
+const gameNotFoundResponse = {
+    error: "Game not found.",
+};
+
+router.get("/", async (_, res) => {
+    const result = await client.query(`SELECT * FROM "Game"`);
+
+    res.send(result.rows);
+});
+
+router.get("/:title", async (req, res) => {
+    const result = await client.query(`SELECT * FROM "Game" WHERE title = $1`, [
+        req.params.title,
+    ]);
+
+    const data = result.rows[0];
+
+    if (!data) res.status(404).send(gameNotFoundResponse);
+
+    res.send(data);
+});
+
+router.get("/:id/tags", async (req, res) => {
+    const result = await client.query(
+        `SELECT * FROM "Tag" WHERE game_id = $1`,
+        [req.params.id]
+    );
+
+    const data = result.rows[0];
+
+    if (!data) res.status(404).send(gameNotFoundResponse);
+
+    const tags = Object.keys(data).filter(
+        key => data[key] !== false && key !== "game_id"
+    );
+
+    res.send({ tags });
 });
 
 export const gameRouter = router;
