b02d2fac65b3416309daf617d48fe9a735691419
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-09 21:34:56 -0800

    Added Prisma ORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,17 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 import { masterRouter } from "./routes/master-router";
+
+const prisma = new PrismaClient();
 
 const main = async () => {
     const app = express();
 
     app.use("/api", masterRouter);
 
-    const port = process.env.PORT;
+    const port = process.env.PORT || 4000;
     app.listen(port, () =>
-        console.log(`server started on http://localhost:${port}/api`)
+        console.log(`api started on http://localhost:${port}/api`)
     );
 };
 
