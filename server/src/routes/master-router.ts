2d5f8aefadf34924bc50f79570f7e8bcdc73699f
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-10 01:17:20 -0800

    Added todo router

--- server/src/routes/master-router.ts
+++ server/src/routes/master-router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { gameRouter } from "./game-routes";
+import { todoRouter } from "./todo-routes";
 
 const router = Router();
 
 router.use("/games", gameRouter);
+router.use("/todos", todoRouter);
 
 export const masterRouter = router;
